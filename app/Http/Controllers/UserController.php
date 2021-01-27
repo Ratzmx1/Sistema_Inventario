7ce0005a5872d02ba4a6bf8b2ca742bf8a36d3c2
Ratzmx <enzoxz.023@gmail.com>
2021-01-27 00:01:04 -0300

    Agrego swagger y un mini ejemplo

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,12 +7,44 @@ use Illuminate\Http\Request;
 use Firebase\JWT\JWT;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * @OA\Info(title="User endpoints", version="1.0")
+ *
+ * @OA\Server(url="localhost:8000")
+ */
 class UserController extends Controller
 {
-    /*
-     *
-     *
+    /**
+     * @OA\Post(
+     *     path="/api/login",
+     *     summary="Inicia Sesion",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Genera Token"
+     *     ),
+     *     @OA\Parameter(
+     *      name="email",
+     *      description="Email",
+     *      in="query",
+     *      required=true,
+     *   ),
+     *    @OA\Parameter(
+     *      name="password",
+     *      description="Contraseña de minimo 6 caracteres",
+     *      in="query",
+     *      required=true,
+     *   ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Usuario no autorizado"
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="Server error"
+     *     )
+     * )
      */
+
     public function login(Request $request){
         $credentials = $request->only(["email","password"]);
         $isValid = Auth::attempt($credentials);
